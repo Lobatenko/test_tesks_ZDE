4f067a853fcdfbdb0b5262b1cf7c2aa4e5c237b2
Lobatenko <dariia.lobatenko@gmail.com>
2021-03-17 19:52:37 +0200

    rewrite input date with input tags

--- main.js
+++ main.js
@@ -3,79 +3,60 @@
     ◦ For example:  d1.daysTo(d2) should return quantity of complete days from d1 to d2
 */
 
-let showDiffOfDays = () => {
-    console.log('Task1');
+document.getElementById('d1').valueAsDate = new Date();
+document.getElementById('d2').valueAsDate = new Date();
 
+let showDiffOfDays = () => {
     var millisecondsPerDay = 24 * 60 * 60 * 1000;
     Date.prototype.msPERDAY = millisecondsPerDay;
     // Return quantity quantity of complete days from this to d
     // If d less than this - return -1
-    Date.prototype.daysTo = function (d) {
-        return d > this ? Math.floor((d-this)/millisecondsPerDay) : -1;
+    Date.prototype.daysTo = function(d) {
+        return d >= this ? Math.floor((d - this) / millisecondsPerDay) : -1;
     };
-    
-    let d1 = new Date(2019,1,15); 
-    let d2 = new Date(2019,1,30);
-    let d3 = new Date(2020,1,30, 3, 24, 0);
-    
-    console.log(d1.daysTo(d2));
-    console.log(d2.daysTo(d1));
-    console.log(d1.daysTo(d3));
-    
-    // Try enter data and calculeted days
-    let today = new Date();
-    let date1 = new Date();
-    let date2 = new Date();   
-    date1 = new Date (prompt("Please enter date1:", today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate()));
-    date2 = new Date (prompt("Please enter date2:", today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate()));
-    
-    if (date1 !== null) {
-        x1 = "Date 1: " + date1;
-        document.getElementById("demo1").innerHTML = x1;
-    }
-    
-    if (date2 !== null) {
-        x2 = "Date 2: " + date2;
-        document.getElementById("demo2").innerHTML = x2;
-    }
 
-    result = "Result: "+date1.daysTo(date2);
-    console.log(result);
+    // Enter data and calculeted days
+
+    let x1 = document.getElementById('d1').value,
+        x2 = document.getElementById('d2').value,
+        date1 = new Date(x1),
+        date2 = new Date(x2);
+
+    result = "Result: " + date1.daysTo(date2);
     document.getElementById("demo-result").innerHTML = result;
-    // alert(result);
+
 }
-
-
+showDiffOfDays();
 
 /*2.2 Develop a program which orders array of sales by amount.
 Input: array of objects with the following structure {amount: 10000, quantity: 10}. 
 Output: new array of ordered sales with the same structure.
 Please note that input shall remain intact.
 */
-    console.log('Task2');
-	let sales = [
-    {amount: 10000, quantity: 10},
-    {amount: 70000, quantity: 1},
-    {amount: 180000, quantity: 3},
-    {amount: 30000, quantity: 5},
-    {amount: 250000, quantity: 8}
-    ];
-    
-    let sortedArr = sales.slice().sort(function(a,b){
+console.log('Task2');
+let sales = [
+    { amount: 10000, quantity: 10 },
+    { amount: 70000, quantity: 1 },
+    { amount: 180000, quantity: 3 },
+    { amount: 30000, quantity: 5 },
+    { amount: 250000, quantity: 8 }
+];
+
+let sortedArr = sales.slice().sort(function(a, b) {
     if (a.amount > b.amount) {
-    	return 1;
+        return 1;
     }
     if (a.amount < b.amount) {
-    	return -1;
+        return -1;
     }
-    return 0;    
-    });
-    
-    console.log(sortedArr);  
+    return 0;
+});
 
-    let showOrdersArray = () => {
-        alert(JSON.stringify(sortedArr, null, 4));
-    }
+console.log(sortedArr);
+
+let showOrdersArray = () => {
+    alert(JSON.stringify(sortedArr, null, 4));
+}
 
 /*        2.3. Develop a program “Object Projection”. 
 Input: any JSON object; prototype object. 
@@ -87,32 +68,31 @@ shall be the same as values of respective properties in source object. */
 console.log('Task3');
 
 let src = {
-	prop11: {
-    	prop21: 21,
+    prop11: {
+        prop21: 21,
         prop22: {
-        	prop31: 31,
+            prop31: 31,
             prop32: 32
-       },
+        },
     },
     prop12: 12,
 };
 prot = {
-	prop11: {
-    	prop22: {
-        	prop32: ""
+    prop11: {
+        prop22: {
+            prop32: ""
         }
     },
     prop13: ""
 };
 
 let projection = (obj, proj) => {
-    let projectedObj = {} 
-    for (let key in proj){
+    let projectedObj = {}
+    for (let key in proj) {
         if (obj[key] !== null && typeof obj[key] === 'object' &&
-            proj[key] !== null && typeof proj[key] === 'object'){
-                projectedObj[key] = projection(obj[key], proj[key]);
-        }
-        else {
+            proj[key] !== null && typeof proj[key] === 'object') {
+            projectedObj[key] = projection(obj[key], proj[key]);
+        } else {
             if (obj.hasOwnProperty(key))
                 projectedObj[key] = obj[key];
         }
@@ -120,11 +100,8 @@ let projection = (obj, proj) => {
     return projectedObj;
 }
 
-console.log(projection(src,prot));
+console.log(projection(src, prot));
 
 let showProjection = () => {
-    alert(JSON.stringify(projection(src,prot), null, 4));
+    alert(JSON.stringify(projection(src, prot), null, 4));
 }
-
-
-
